b21a3f918df5a883ff452492705a0eea3225603e
gabbya8 <gabriellaalexis2399@gmail.com>
2021-09-05 21:50:14 -0400

    Updates

---
+++ precache-manifest.70c8445dc446720043f5ce7726292563.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5086d5bba656311f5dc90fed82afadd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960ed2b4a8211b73d175",
+    "url": "/static/css/main.9bfbe2f9.chunk.css"
+  },
+  {
+    "revision": "0a9e28e4e7c31eb58b3f",
+    "url": "/static/js/2.fa10362e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.fa10362e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960ed2b4a8211b73d175",
+    "url": "/static/js/main.b7cfaa35.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f3b738ba61f300a32492e67f325b33c0",
+    "url": "/static/media/climate-game.f3b738ba.png"
+  },
+  {
+    "revision": "10a6aa026820e2c15c132c0b9ed8fc57",
+    "url": "/static/media/explore-page.10a6aa02.png"
+  },
+  {
+    "revision": "0767565f001a993eaac2f546bc6bf3bb",
+    "url": "/static/media/gwc-logo.0767565f.png"
+  },
+  {
+    "revision": "7bd5f0770b7a425d6abf42904105ad92",
+    "url": "/static/media/linkedinpic.7bd5f077.jpeg"
+  },
+  {
+    "revision": "a8ab90a32ea5cf3ba850d3a53a985d99",
+    "url": "/static/media/mindweaver.a8ab90a3.png"
+  },
+  {
+    "revision": "ba2dd9a68d4ed7966c049b167a490f79",
+    "url": "/static/media/music-sample-site.ba2dd9a6.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "13ccd2b8c9a8fa49e91981830e349d31",
+    "url": "/static/media/star-wars.13ccd2b8.png"
+  }
+]);
